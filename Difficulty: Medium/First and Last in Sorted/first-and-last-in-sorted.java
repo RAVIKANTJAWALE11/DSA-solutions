7d1a1d2d46974ed3a767afd5cd36ca94fc1b552e
RAVIKANTJAWALE11 <ravikantjawale36@gmail.com>
2026-06-24 18:33:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last in Sorted/first-and-last-in-sorted.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class Solution {
+    ArrayList<Integer> find(int arr[], int x) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int first = -1, last = -1;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                if (first == -1) first = i;  
+                last = i; 
+            }
+        }
+        
+        res.add(first);
+        res.add(last);
+        return res;
+    }
+}
