ca43c7ff59c802522211a38dd16d3fcef249472d
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-06-26 10:25:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Boolean Matrix/boolean-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    void booleanMatrix(int mat[][]) {
+        // code here
+        int n = mat.length;
+        int m = mat[0].length;
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(mat[i][j]==1){
+                    row[i]=true;
+                    col[j]=true;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(row[i]==true ||col[j]==true) mat[i][j]=1;
+            }
+        }
+        
+    }
+}
