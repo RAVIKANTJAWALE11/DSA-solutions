256274c084e64b502d5a7fc9f6388a7c3cfcda83
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-06-27 11:05:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitonic Point/bitonic-point.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findMaximum(int[] arr) {
+        // code here
+        int max = 0;
+        for(int i =0 ;i<arr.length;i++){
+            if(arr[i]>max) max = arr[i];
+        }
+        return max;
+    }
+}
