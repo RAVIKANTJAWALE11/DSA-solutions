e37e82022f8b6e998c3b51aaff79393284e5590d
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-06-29 16:43:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Dot Product with 0 Insertions/max-dot-product-with-0-insertions.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxDotProduct(int[] A, int[] B) {
+        // code here
+        int n = A.length;
+        int m = B.length;
+        int[] dp = new int[m + 1];
+        Arrays.fill(dp, Integer.MIN_VALUE / 2); 
+        dp[0] = 0; 
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                int skipA = dp[j]; 
+                int pairBoth = dp[j - 1] + (A[i - 1] * B[j - 1]);
+
+                dp[j] = Math.max(skipA, pairBoth);
+            }
+        }
+
+        return dp[m];
+    }
+    
+}
