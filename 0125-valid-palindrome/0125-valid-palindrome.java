505415a0f2cb0a1289ecdba9a6d4c58ca0ca5ef7
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-07-06 10:11:51 +0530

    Time: 125 ms (7.48%), Space: 48 MB (7.5%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        String res = "";
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch >= 65 && ch <= 90) {
+                ch = (char)(ch + 32);
+                res += ch;
+            }
+            else if (ch >= 97 && ch <= 122) {
+                res += ch;
+            }
+            else if (ch >= 48 && ch <= 57) {
+                res += ch;
+            }
+        }
+        int m = res.length();
+        for (int j = 0; j < m / 2; j++) {
+            if (res.charAt(j) != res.charAt(m - j - 1)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
