92d43e0b4dcb757e6215e7e7cca448b9735f87b7
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-07-06 22:49:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common Elements/common-elements.java
@@ -0,0 +1,27 @@
+class Solution {
+	public static ArrayList<Integer> commonElements(int a[], int b[]) {
+		ArrayList<Integer> res = new ArrayList<>();
+		
+		// 1. Sort both arrays to use the two-pointer approach
+		Arrays.sort(a);
+		Arrays.sort(b);
+		
+		int i = 0;
+		int j = 0;
+		
+		while (i < a.length && j < b.length) {
+			if (a[i] == b[j]) {
+				res.add(a[i]);
+				i++;
+				j++;
+			} else if (a[i] < b[j]) {
+				
+				i++;
+			} else {
+				
+				j++;
+			}
+		}
+		return res;
+	}
+}
