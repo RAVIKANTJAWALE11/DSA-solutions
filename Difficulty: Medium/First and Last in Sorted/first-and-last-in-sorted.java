16adcf708a6118dd47c2f0fe222a719d280d6b5b
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-07-08 23:00:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last in Sorted/first-and-last-in-sorted.java
+++ Difficulty: Medium/First and Last in Sorted/first-and-last-in-sorted.java
@@ -3,17 +3,47 @@ import java.util.ArrayList;
 class Solution {
     ArrayList<Integer> find(int arr[], int x) {
         ArrayList<Integer> res = new ArrayList<>();
-        int first = -1, last = -1;
         
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == x) {
-                if (first == -1) first = i;  
-                last = i; 
+        res.add(findFirst(arr, x));
+        res.add(findLast(arr, x));
+        
+        return res;
+    }
+    
+    private int findFirst(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int first = -1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] == x) {
+                first = mid;       
+                high = mid - 1;  
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
+        return first;
+    }
+    private int findLast(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int last = -1;
         
-        res.add(first);
-        res.add(last);
-        return res;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] == x) {
+                last = mid;        
+                low = mid + 1;     
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return last;
     }
 }
