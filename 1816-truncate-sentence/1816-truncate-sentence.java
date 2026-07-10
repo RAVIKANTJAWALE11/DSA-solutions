e60d5437dd0e3c370b18fcde5e3516196c813525
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-07-10 09:13:12 +0530

    Time: 0 ms (100%), Space: 41.9 MB (100%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,9 +1,10 @@
 class Solution {
     public String truncateSentence(String s, int k) {
+        int count = k;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ' ') {
-                k--;
-                if (k == 0) {
+                count--;
+                if (count == 0) {
                     return s.substring(0, i);
                 }
             }
