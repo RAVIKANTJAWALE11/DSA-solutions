14919eec8d81d0d6b4ef22d71c99f35ea9f48afa
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-07-13 09:38:40 +0530

    Time: 0 ms (100%), Space: 44.8 MB (48.26%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] arr, int k) {
+            int n = arr.length;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]==k) return mid;
+            else if(arr[mid]>k) high = mid-1;
+            else low = mid+1;
+        }
+        return high+1;
+    }
+}
