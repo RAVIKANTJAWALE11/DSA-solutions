d9cc517f9301ed7882c8b296765d55947ed12347
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-07-31 18:25:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a Row-Column Sorted/search-in-a-row-column-sorted.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static boolean matSearch(int matrix[][], int target) {
+        // code here
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n-1;
+        while(row<m && col>=0){
+            int val = matrix[row][col];
+            if(val==target) return true;
+            else if(val>target) col--;
+            else row++;
+        }
+        return false;
+    }
+}
