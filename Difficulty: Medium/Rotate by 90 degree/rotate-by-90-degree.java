25defbc890f9a2736326a7c58e1fc9309e327428
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-08-04 21:35:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotateMatrix(int[][] arr) {
+        // Reverse Each Row
+        for(int i=0;i<arr.length;i++){
+            int stCol = 0, endCol = arr[0].length - 1;
+            while(stCol<endCol){
+                int temp = arr[i][stCol];
+                arr[i][stCol] = arr[i][endCol];
+                arr[i][endCol] = temp;
+                stCol++;
+                endCol--;
+            }
+        }
+           for(int i=1;i<arr.length;i++){
+            for(int j=0;j<i;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+}
