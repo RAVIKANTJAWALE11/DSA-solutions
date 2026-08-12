2b0ddd7625be0c2e5598553f968d1039f57e5d07
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-08-12 18:25:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int[] productExceptSelf(int arr[]) {
+        // code here
+        int n = arr.length;
+        int [] res = new int [n];
+        res[0] = 1;
+        for(int i = 1 ; i< n ; i++){
+            res[i] = res[i-1] * arr[i-1];
+        }
+        int mul = 1;
+        for(int i = n-1;i>=0;i--){
+            res[i] *=mul;
+            mul *= arr[i];
+        }
+        return res;
+    }
+}
