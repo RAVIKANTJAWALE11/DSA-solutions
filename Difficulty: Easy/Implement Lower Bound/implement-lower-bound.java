07f3f4e926cdfd036fb473fed70635204dceb2e1
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-08-31 08:14:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -6,8 +6,7 @@ class Solution {
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (arr[mid] >= target) {
+         if (arr[mid] >= target) {
                 ans = mid;       
                 high = mid - 1;
             } else {
