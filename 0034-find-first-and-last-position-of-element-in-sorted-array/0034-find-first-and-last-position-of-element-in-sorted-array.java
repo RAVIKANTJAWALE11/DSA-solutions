1052b58b9a6d5c382c5feafa8df65408db504d5c
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-08-31 09:12:19 +0530

    Time: 0 ms (100%), Space: 48.4 MB (14.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,45 @@
 class Solution {
     public int[] searchRange(int[] arr, int target) {
+        int x = first(arr, target);
+        int y = last(arr, target);
+        return new int[] { x, y };
+    }
+
+    static int first(int[] arr, int target) {
         int n = arr.length;
+        int low = 0, high = n - 1;
         int one = -1;
-        int last = -1;
-        int high = n - 1;
-        int low = 0;
-        //1st occurance ka code 
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (arr[mid] > target)
+            if (arr[mid] > target) {
                 high = mid - 1;
-            else if (arr[mid] < target)
+            } else if (arr[mid] < target) {
                 low = mid + 1;
-            else {
+            } else {
                 one = mid;
-                high = mid - 1;
+                high = mid - 1; 
             }
         }
+        return one;
+    }
 
-        //last occurance 
-        low = 0;
-        high = n - 1;
+    static int last(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int last = -1;
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
             if (arr[mid] == target) {
                 last = mid;
-                low = mid + 1;
+                low = mid + 1; 
             } else if (arr[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return new int[] { one, last };
-
+        return last;
     }
 }
