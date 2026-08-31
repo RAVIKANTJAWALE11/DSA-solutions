a9089eb45b10db61cadd2b6907e143afdaba9ca2
Ravikant Jawale <ravikantjawale36@gmail.com>
2026-08-31 07:52:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count 1's in binary array/count-1s-in-binary-array.java
+++ Difficulty: Easy/Count 1's in binary array/count-1s-in-binary-array.java
@@ -1,26 +1,11 @@
 class Solution {
     public int countOnes(int[] arr) {
+        // code here
         int n = arr.length;
-        
-        if (n == 0 || arr[0] == 0) {
-            return 0;
+        int count = 0 ;
+        for(int i = 0 ; i<n;i++){
+            if(arr[i]==1) count++;
         }
-
-        int low = 0;
-        int high = n - 1;
-        int last = 0;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (arr[mid] == 1) {
-                last = mid;   
-                low = mid + 1; 
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return last + 1;
+        return count;
     }
 }
